cd238bfde6d9e00d5f0287d944b100f6f0b96cee
mclaramarinho <marinho.claramb@gmail.com>
2026-06-08 22:28:16 -0300

    feat: Implement PasswordRecoveryPage with email notification message and scaffold layout

--- lib/features/auth/password_recovery/presentation/pages/password_recovery_email_sent_page.dart
+++ lib/features/auth/password_recovery/presentation/pages/password_recovery_email_sent_page.dart
@@ -1,5 +1,23 @@
+import 'package:adhd_app/shared/design_system/widgets/ds_button/ds_button.dart';
+import 'package:adhd_app/shared/design_system/widgets/ds_scaffold/ds_scaffold.dart';
 import 'package:flutter/material.dart';
 
-// class PasswordRecoveryPage extends StatelessWidget {}
-
-// class _PasswordRecoveryPageContent extends StatelessWidget {}
+class PasswordRecoveryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DsScaffold(
+      children: [
+        Text("Check yout email!"),
+        Text(
+          "If an account is linked to that email, you will receive a link to reset your password.",
+        ),
+        // TODO - implement this functionality
+        // DsButton.textAndLink(
+        //   onPressed: () {},
+        //   label: "Having trouble resetting your password?",
+        //   linkLabel: "Contact an admin!",
+        // ),
+      ],
+    );
+  }
+}
