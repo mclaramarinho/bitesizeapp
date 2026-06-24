a0cc54f14244413da30b300a6c7dea06c10ddd9b
mclaramarinho <marinho.claramb@gmail.com>
2026-06-24 13:56:21 -0300

    feat: Update DsBottomNavbar to use routes instead of indexes and improve layout

--- lib/shared/design_system/widgets/ds_bottom_navbar/ds_bottom_navbar.dart
+++ lib/shared/design_system/widgets/ds_bottom_navbar/ds_bottom_navbar.dart
@@ -1,40 +1,46 @@
 import 'package:adhd_app/shared/design_system/constants/ds_spacing.dart';
 import 'package:adhd_app/shared/design_system/constants/ds_themes.dart';
+import 'package:adhd_app/shared/di/injection.dart';
+import 'package:adhd_app/shared/utils/navigation/router.dart';
+import 'package:adhd_app/shared/utils/navigation/routes.dart';
 import 'package:flutter/material.dart';
 
 class DsBottomNavbar extends StatelessWidget {
+  /// Ordered from left-right
   final List<BottomNavbarItem> items;
-  final int currentSelection;
 
-  DsBottomNavbar({
-    required this.items,
-    required this.currentSelection,
-    super.key,
-  }) : assert(
-         items.map((item) => item.index).toSet().length == items.length,
-         "Items indexes must be different.",
-       );
-
+  DsBottomNavbar({required this.items, super.key})
+    : assert(
+        items.map((item) => item.route).toSet().length == items.length,
+        "Items routes must be different.",
+      );
   @override
   Widget build(BuildContext context) {
+    final router = getIt.get<AppRouter>();
+    final currentRoute = router.matchedLocation;
     return FittedBox(
       fit: BoxFit.scaleDown,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: List.from(
-          items.map((item) {
-            return IconButton(
-              onPressed: item.onTap,
-              icon: Icon(item.icon),
-              padding: EdgeInsets.symmetric(
-                vertical: DsSpacing.none,
-                horizontal: DsSpacing.sm,
-              ),
-              isSelected: item.index == currentSelection,
-              tooltip: item.tooltip,
-              highlightColor: DsThemes.current.colorScheme.onPrimaryContainer,
-            );
-          }),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: DsSpacing.md),
+        width: MediaQuery.sizeOf(context).width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.from(
+            items.map((item) {
+              return IconButton(
+                onPressed: item.onTap,
+                icon: Icon(item.icon),
+                padding: EdgeInsets.symmetric(
+                  vertical: DsSpacing.none,
+                  horizontal: DsSpacing.sm,
+                ),
+                iconSize: DsSpacing.xxl,
+                isSelected: item.route == currentRoute,
+                tooltip: item.tooltip,
+                highlightColor: DsThemes.current.colorScheme.onPrimaryContainer,
+              );
+            }),
+          ),
         ),
       ),
     );
@@ -45,12 +51,12 @@ class BottomNavbarItem {
   final IconData icon;
   final String tooltip;
   final VoidCallback onTap;
-  final int index;
+  final AppRoutes route;
 
   const BottomNavbarItem({
     required this.icon,
     required this.tooltip,
     required this.onTap,
-    required this.index,
+    required this.route,
   });
 }
